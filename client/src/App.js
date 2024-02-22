a7d6d07d5db2d05999ea3bdf3298d887843c2f5b
Hassan Raza <hassanraza0140@gmail.com>
2024-02-22 16:27:16 -0600

    Update Directory structure

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h3 className="text-3xl text-center text-bold">Jango Ecommerce Store</h3>
+  );
+}
+
+export default App;
--- src/App.js
+++
@@ -1,7 +0,0 @@
-const App = () => {
-  return (
-    <h3>Jango Ecommerce Store</h3>
-  );
-}
-
-export default App;
